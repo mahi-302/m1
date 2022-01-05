9a723245151e99f395b10faf8c54bd194656a9bf
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-06 00:27:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws"{
   region = "us-east-2"
 }
 module "A" {
-  source = "git::https://github.com/mahi-302/module.git.?ref=master2"
+  source = "git::https://github.com/mahi-302/module.git.?ref=master"
 }
   module "B" {
-    source = "git::https://github.com/mahi-302/module.git?ref=master"
+    source = "git::https://github.com/mahi-302/module.git?ref=master2"
     security_groups = var.security_groups
       target_group_arns = [module.A.TG_ARN]
  }
