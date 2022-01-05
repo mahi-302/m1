74396a8b7b766a7f1c09cbf1b4a5bff2b2845a5e
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-06 01:03:07 +0530

    Update modulesg.tf

--- modulesg.tf
+++ modulesg.tf
@@ -1,4 +1,4 @@
-resource "aws_launch_configuration" "as_conf" {
+resource "aws_launch_configuration" "aslc" {
   name_prefix   = var.name_prefix
   image_id      = var.image_id
   instance_type = var.instance_type
@@ -6,8 +6,8 @@ resource "aws_launch_configuration" "as_conf" {
   }
 resource "aws_autoscaling_group" "bar" {
   name                 = var.name 
-  depends_on           = ["aws_launch_configuration.as_conf","aws_lb_target_group.test"]
-  launch_configuration = aws_launch_configuration.as_conf.name
+  depends_on           = ["aws_launch_configuration.aslc","aws_lb_target_group.test"]
+  launch_configuration = aws_launch_configuration.aslc.name
   min_size             = 1
   max_size             = 2
   desired_capacity     = 1
