2aaeb2035d6165a181b581445cf46e2576f8bf73
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-06 17:10:07 +0530

    Update code.tf

--- code.tf
+++ code.tf
@@ -1,9 +1,6 @@
 provider "aws"{
   region = "us-east-2"
 }
-module "main"{
-    source = "git::https://github.com/mahi-302/m1.git?ref=master2"
-}
 module "lb"{
     source = "git::https://github.com/mahi-302/m1.git?ref=main"
 }
