be36880e7ed37507a119a09e24be29793c23f6c5
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-06 16:34:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,5 @@ module "A" {
 }
   module "B" {
     source = "git::https://github.com/mahi-302/module.git?ref=master2"
-    security_groups = var.security_groups
-      target_group_arns = [module.A.TG_ARN]
+      target_group_arns = module.A.TG_ARN
  }
